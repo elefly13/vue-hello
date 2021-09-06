e0cde804bebd06976a0dd1d9fcbf801bf5ea2ad0
Elefly13 <elymara13@gmail.com>
2021-09-06 16:46:33 +0200

    prima parte esercizio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 Vue.config.devtools = true;
+
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const app = new Vue (
+    {
+        el: '#root',
+        data: {
+            message: 'Benvenuto nel mondo VUE 3 JS',
+            
+
+        }
+
+})
+
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
